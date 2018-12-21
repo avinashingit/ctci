0ccf7a5bb10d20dcab788a3143d45bf61d7db02e
avinashkadimisetty <avinash.k@evivehealth.com>
2018-12-20 21:05:38 -0600

    Modified merge sorted arrays code

---
+++ MergeArrays/merge_sorted_arrays.py
@@ -0,0 +1,24 @@
+def merge_sorted_arrays(list1, list2):
+    total_length = len(list1) + len(list2)
+    sorted_array = []
+    i, j = 0, 0
+    while i+j < total_length:
+        if i == len(list1):
+            sorted_array.append(list2[j])
+            j += 1
+        elif j == len(list2):
+            sorted_array.append(list1[i])
+            i += 1
+        elif list1[i] < list2[j]:
+            sorted_array.append(list1[i])
+            i += 1
+        elif list1[i] >= list2[j]:
+            sorted_array.append(list2[j])
+            j += 1
+        print(i, j)
+    return sorted_array
+
+
+l1 = [3, 4, 6, 10, 11, 15]
+l2 = [1, 5, 8, 12, 14, 16]
+print(merge_sorted_arrays(l1, l2))
