0756e28d46cbcb5fb4393d8a924f838d187a0476
Avinash Kadimisetty <kavinash366@gmail.com>
2018-12-21 00:36:55 -0600

    Code for merging k sorted arrays added

--- MergeArrays/merge_multiple_sorted_arrays.py
+++ MergeArrays/merge_multiple_sorted_arrays.py
@@ -19,6 +19,21 @@ def merge_sorted_arrays(list1, list2):
     return sorted_array
 
 
+def merge_multiple_arrays(arrays):
+    if len(arrays) == 1:
+        return arrays
+    else:
+        new_arrays = []
+        for i in range(0, len(arrays), 2):
+            if i == len(arrays)-1:
+                new_arrays.append(arrays[i])
+            else:
+                new_arrays.append(merge_sorted_arrays(arrays[i], arrays[i+1]))
+        return merge_multiple_arrays(new_arrays)
+
+
 l1 = [3, 4, 6, 10, 11, 15]
 l2 = [1, 5, 8, 12, 14, 16]
-print(merge_sorted_arrays(l1, l2))
+l3 = [1, 2, 3, 4, 5, 50]
+l4 = [23, 24, 25, 29, 30, 31]
+print(merge_multiple_arrays([l1, l2, l3, l4]))
